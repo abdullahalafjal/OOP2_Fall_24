c9f8ca4316c90644c967096f1556938cae2164a0
Abdullah Al Afjal <159076048+abdullah-al-afjal@users.noreply.github.com>
2024-07-15 23:45:20 +0600

    Add files via upload

---
+++ FirstProgram.py
@@ -0,0 +1,9 @@
+# single line comments
+'''
+duble line Comments
+ '''
+
+print("\n\tAbdullah Al Afjal", "CSE", "Daffodil Internation Univercity",  "24")
+print("Abdullah Al Afjal\nCSE \nDaffodil Internation Univercity \n24")
+print(1794632235)
+print("\t01794632235")
