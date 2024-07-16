4969e38bcb687fbd19d334c2b7ed7ee9eb00acce
Abdullah Al Afjal <159076048+abdullah-al-afjal@users.noreply.github.com>
2024-07-16 10:37:08 +0600

    Add files via upload

---
+++ Program.py
@@ -0,0 +1,29 @@
+name = "Abdullah AL Afjal"
+age=34
+cgpa=3.50
+
+print("My name is :",name,age,cgpa)
+print("My age:",age)
+print ("My CGPA:",cgpa)
+
+print("Abdullah Al " + "Afjal")
+
+A=23
+B=4
+
+print(A+B)
+print(A-B)
+print(A/B)
+print(A%B)
+print(A*B)
+print("\n")
+print(A//B)  #float ber kore like 4.6556  result 4
+
+num1=30
+num2=30
+print(num1+num2)
+
+print(f"{num1} + {num2} ={num1+num2}")  #formatted
+
+print("Abdullah",end=" ")  #eki line print kora
+print("02323429494")
