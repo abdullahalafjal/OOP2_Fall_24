70940dfde71e5aa33976583b4ad0edf98570af26
Abdullah Al Afjal <159076048+abdullah-al-afjal@users.noreply.github.com>
2024-08-23 02:11:45 +0600

    Add files via upload

---
+++ Statement.py
@@ -0,0 +1,27 @@
+# pass or fail
+mark=70
+if mark>=33:
+    print("Pass")
+else:
+    print("Fail")
+
+
+# max number
+num1=40
+num2=20
+
+if num1>num2:
+    print(num1)
+else:
+    print(num2)
+
+
+number = 0
+if number > 0:
+    print('positive number')
+elif number < 0:
+    print('negative number')
+else:
+    print('zero')
+
+print('This statement is always executed')
