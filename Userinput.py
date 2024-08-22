28c275545d297121c5469d0b2a7909ab24632940
Abdullah Al Afjal <159076048+abdullah-al-afjal@users.noreply.github.com>
2024-08-23 02:13:34 +0600

    Add files via upload

---
+++ Userinput.py
@@ -0,0 +1,10 @@
+name = input("Enter your name:")
+age = input("Enter your Age :")
+gpa =input("Enter your gpa:")
+
+print("Student Information")
+print("-------------------")
+print("Name:"+name)
+print("Name:",age)
+print("Name:",gpa)
+
