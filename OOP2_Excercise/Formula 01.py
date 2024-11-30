281f097f34478fe68c87099bf7de4f5cb0230c34
Abdullah Al Afjal <159076048+abdullahalafjal@users.noreply.github.com>
2024-11-30 22:11:23 +0600

    Add files via upload

---
+++ OOP2_Excercise/Formula 01.py
@@ -0,0 +1,8 @@
+def formula():
+    a = int(input("Enter first number:"))
+    b = int(input("Enter second number:"))
+    return a**2 + b**2 + 2 * a * b
+
+
+result = formula()
+print(result)
---
+++ OOP2_Excercise/Lamda Function 02.py
@@ -0,0 +1,2 @@
+result = lambda a, b: a**2 + b**2 + 2 * a * b
+print(result(5, 3))
